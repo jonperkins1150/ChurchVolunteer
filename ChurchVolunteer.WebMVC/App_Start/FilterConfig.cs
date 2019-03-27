731f88d606dbdb5687193bd2b1d0b4ed49cd2306
Jon Perkins <jonperkins1150@gmail.com>
2019-03-27 11:26:21 -0400

    Add project files.

---
+++ ChurchVolunteer.WebMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ChurchVolunteer.WebMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ChurchVolunteer.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChurchVolunteer.WebMVC.Startup))]
+namespace ChurchVolunteer.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
